d513274e72460e838711720108e0b8fda856c4f1
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-07-11 17:38:31 +0200

    all basic functionality implemented using jQuery

---
+++ README.md
@@ -0,0 +1,24 @@
+--- Project setup documentation ---
+
+General purpose of the README:
+* what it is, how to set it up, which development environment to use, what dependencies to watch out for, etc.
+* technical requirements to the libraries used, necessary tools, and connected database
+
+Useful links:
+* https://guides.github.com/features/mastering-markdown/
+* https://jquery.com/
+* list of open APIs: https://public-apis.xyz/
+
+For this app:
+* Project description: A simple ToDo List App
+* Features:
+  * The user can add a new item to a list of items.
+  * The user can cross out an item from the list of items.
+  * The user can delete an item from the list of items.
+  * The user can change the order of items in the list of items.
+
+* Project dependencies (e.g., JavaScript version, ESLint rules)
+  * jQuery
+
+* Which API the project uses
+  * 
--- js/Scripts.js
+++ js/Scripts.js
@@ -1,7 +1,53 @@
+// jQuery version
 
 function newItem(){
+    let list = $('#list');
+    
+    // Reordering the items: 
+    list.sortable();
 
-    //javascript
+    // Adding a new item to the list
+    let inputValue = $('#input').val();
+
+    if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        let li = $('<li></li>')
+        li.append(inputValue);
+        list.append(li);
+
+        // let newLi = $('<li>inputValue</li>');
+        // list.append(newLi);
+
+        // Crossing out an item from the list
+        li.on("click", function() {
+            li.toggleClass("strike");
+        });
+
+        // adding the delete button "X"
+        let deleteButton = $('<crossOutButton></crossOutButton>');
+        deleteButton.append(document.createTextNode('X'));
+        li.append(deleteButton);
+        deleteButton.on("click", function(){
+            li.fadeOut(1000);
+            // li.addClass('delete'); // Maybe a promise would make sense for the fadeout ?
+            // let items = $('li');
+            // console.log(items);
+        });
+        //deleteButton.on("click", deleteListItem);
+        /*
+            // Adding CLASS DELETE (DISPLAY: NONE) from the css
+            function deleteListItem(){
+            li.addClass('delete');
+            }
+        */
+      }    
+}
+
+/*
+// BELOW PURE VANILLA JS (for comparison)
+function newItem(){
+
     //1. Adding a new item to the list of items: 
        let li = document.createElement("li");
        let inputValue = document.getElementById("input").value;
@@ -35,8 +81,9 @@ function newItem(){
      // 4. Reordering the items: 
        $('#list').sortable();
     
-    }
-   
+    } 
+*/
+
     // If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
     
     
